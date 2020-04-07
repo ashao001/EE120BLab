036e840e24599059517f13fc686bc8255226bad2
Astraea Shao <ashao001@wch136-17.cs.ucr.edu>
2020-04-07 15:17:03 -0700

    Initializing repository with lab files

---
+++ turnin/ashao001_lab2_intro.c
@@ -0,0 +1,26 @@
+/*
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment:Lab #2  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+     DDRB = 0xFF; //CONFIGURE 8 PINS AS OUPUT
+     PORTB = 0X00;  //INITIALIZE
+     DDRA = 0x00; //input
+     PORTA = 0xFF;
+    /* Insert your solution below */
+    while (1) {
+	PORTB = 0x0F;
+    }
+    return 1;
+}
