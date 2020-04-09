9e54874dea988a4fc4361c9cdfd6a48b86bbd40f
Astraea Shao <ashao001@wch136-17.cs.ucr.edu>
2020-04-09 00:06:05 -0700

    add main p4

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*
  *  Partner(s) Name: none
  *	Lab Section: 023
- *	Assignment:Lab #2  Exercise #3
+ *	Assignment:Lab #2  Exercise #4
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -15,57 +15,38 @@
 int main(void) {
     /* Insert DDR and PORT initializations */
      
-     DDRC = 0xFF; //CONFIGURE 8 PINS AS OUPUT
-     PORTC = 0X00;  //INITIALIZE
+     DDRD = 0xFF; //CONFIGURE 8 PINS AS OUPUT
+     PORTD = 0X00;  //INITIALIZE
      DDRA = 0x00; //input
      PORTA = 0xFF;
+     DDRB = 0x00; //input
+     PORTB = 0xFF;
+     DDRC = 0x00; //input
+     PORTC = 0xFF;
     /* Insert your solution below */
-     unsigned char tempA, tempB, tempC, tempD, cntavail;
-     cntavail = 0;
+     unsigned char max, difference;
+     max = 140;
+    // difference = PINA - PINB;
 
     while (1) {
 	//cntavail = 0;
-	tempA = PINA & 0x01;
-	tempB = PINA & 0x02;
-	tempC = PINA & 0x04;
-	tempD = PINA & 0x08;
-	if(tempA == 0x01){
-		cntavail = cntavail + 1;
+	//tempA = PINA & 0x01;
+	//tempB = PINA & 0x02;
+	//tempC = PINA & 0x04;
+	//tempD = PINA & 0x08;
+	int total;                                                                                                                                                                                                         total = PINA + PINB + PINC;
+        total = total / 3;                                                                                                                                                                                                 PORTD = total; 
+	PORTD = PORTD <<2;
+
+	difference = PINA - PINC;
+	if(PINA + PINB + PINC >= max){
+		PORTD =PORTD |  0x01;
 		
 }
-	if(tempB == 0x02){
-                cntavail = cntavail + 1;
-	
+	if(difference >= 80){
+                PORTD = PORTD | 0x02;
 }
-	if(tempC == 0x04){
-                cntavail = cntavail + 1;
-}
-	if(tempD == 0x08){
-                cntavail = cntavail + 1;
-
-}
-	
-	if (4 - cntavail == 0){
-		PORTC = PORTC | 0x80;
-		PORTC = PORTC & 0X80;
-} 
-	else if(4 - cntavail == 1){
-		PORTC = PORTC | 0x01;
-		PORTC = PORTC & 0X01;
-}
-	else if(4 - cntavail == 2){
-                PORTC = PORTC | 0x02;
-                PORTC = PORTC & 0X02;
-}
-	else if(4 - cntavail == 3){
-                PORTC = PORTC | 0x03;
-                PORTC = PORTC & 0X03;
-}
-	else if(4 - cntavail == 4){
-                PORTC = PORTC | 0x04;
-                PORTC = PORTC & 0X04;
-}
-	cntavail = 0;
+	total = 0;
     }
 
     return 1;
